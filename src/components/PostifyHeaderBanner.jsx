71270376e7720f755736d01f5866fc0f982e17a4
Aditya singh <144699096+aditya265254@users.noreply.github.com>
2026-08-12 13:17:20 +0530

    feat: add dark mode support to PostifyHeaderBanner with new image asset

--- src/components/PostifyHeaderBanner.jsx
+++ src/components/PostifyHeaderBanner.jsx
@@ -1,14 +1,37 @@
-import React from "react";
 import headerGraphicImg from "../assets/img.png";
 
+import headerGraphicImgDark from "../assets/img-dark.png";
+
+
+import { useTheme } from "../context/ThemeContext";
+
 export const PostifyHeaderBanner = ({ className = "" }) => {
+
+const context =  useTheme()
+console.log(context)
+console.log("Context", headerGraphicImgDark)
+
   return (
     <div className={`relative flex items-center justify-center p-2 rounded-xl ${className}`}>
-      <img
+
+{
+  context.theme === "dark" ?     <img
+        src={headerGraphicImgDark}
+        alt="Postify Header Artwork"
+        className="w-full max-w-[260px] sm:max-w-[290px] aspect-square rounded-full h-auto object-contain dark:opacity-90 dark:mix-blend-luminosity transition-all duration-300 drop-shadow-md fade-in"
+      /> 
+      :     <img
         src={headerGraphicImg}
         alt="Postify Header Artwork"
         className="w-full max-w-[260px] sm:max-w-[290px] aspect-square rounded-full h-auto object-contain dark:opacity-90 dark:mix-blend-luminosity transition-all duration-300 drop-shadow-md"
       />
+}
+
+      {/* <img
+        src={headerGraphicImg}
+        alt="Postify Header Artwork"
+        className="w-full max-w-[260px] sm:max-w-[290px] aspect-square rounded-full h-auto object-contain dark:opacity-90 dark:mix-blend-luminosity transition-all duration-300 drop-shadow-md"
+      /> */}
     </div>
   );
 };
--- src/components/PostifyHeroSection.jsx
+++ src/components/PostifyHeroSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { X, Sparkles, Pause } from "lucide-react";
 import PostifyHeaderBanner from "./PostifyHeaderBanner.jsx";
 
